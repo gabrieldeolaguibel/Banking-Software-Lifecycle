bfa51702f2765901c60a05def987df8876900221
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-10-11 23:26:44 +0200

    Added tests: 2 functional and two Unit

--- ie-bank-be/tests/functional/test_routes.py
+++ ie-bank-be/tests/functional/test_routes.py
@@ -1,4 +1,6 @@
 from iebank_api import app
+from iebank_api.models import Account
+from app import db
 import pytest
 
 def test_get_accounts(testing_client):
@@ -28,5 +30,29 @@ def test_create_account(testing_client):
     """
     response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€', 'country': 'CountryName'}) # Added country
     assert response.status_code == 200
+
+# This test will check if accessing a specific account via its ID returns the correct account details.
+def test_get_single_account(client):
+    # Create a dummy account
+    account = Account(name="Test Account", currency="USD", country="USA")
+    db.session.add(account)
+    db.session.commit()
+
+    # Fetch the account details via the API
+    response = client.get(f"/accounts/{account.id}")
+
+    # Assert that the response contains the correct account details
+    assert response.status_code == 200
+    assert response.json["name"] == "Test Account"
+
+# This test will check if accessing an account that doesn't exist returns a 404 status code.
+def test_account_not_found(client):
+    # Try to fetch an account that doesn't exist
+    response = client.get("/accounts/9999")
+
+    # Assert that the response returns a 404 status code
+    assert response.status_code == 404
+
+
 
 
