53ba81ffe6f823ac62f056d7d30cd91cb6e0f6f5
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-10-11 23:45:20 +0200

    Trying to debug tests

--- ie-bank-be/tests/conftest.py
+++ ie-bank-be/tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from iebank_api.models import Account
 from iebank_api import db, app
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 
 @pytest.fixture
