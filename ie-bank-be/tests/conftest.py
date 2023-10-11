2ac9035e303d7e576a193da65d23d45e3cecfb00
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-10-12 00:20:02 +0200

    Debugging

--- ie-bank-be/tests/conftest.py
+++ ie-bank-be/tests/conftest.py
@@ -3,7 +3,19 @@ from iebank_api.models import Account
 from iebank_api import db, app
 
 
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
+
+@pytest.fixture(scope='function')
+def session():
+    db.create_all()
+    yield db.session
+    db.session.remove()
+    db.drop_all()
 
 @pytest.fixture
 def testing_client(scope='module'):
