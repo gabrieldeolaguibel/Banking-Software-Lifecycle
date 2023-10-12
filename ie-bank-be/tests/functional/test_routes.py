663e283e9b886fd32480d6432b4cd85274497f01
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-10-12 22:53:01 +0200

    2 new functional tests

--- ie-bank-be/tests/functional/test_routes.py
+++ ie-bank-be/tests/functional/test_routes.py
@@ -37,34 +37,25 @@ def test_create_account(testing_client):
     assert response.status_code == 200
 
 
-"""
-# Test for Creating a New Account with Valid Data:
-def test_create_new_account(client):
-    data = {
-        "name": "John Doe",
-        "currency": "€",
-        "country": "Spain"
-    }
-    response = client.post("/accounts", json=data)
+# New test 1:
+def test_update_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<int:id>' page is posted to (PUT)
+    THEN check the response is valid
+    """
+    response = testing_client.put('/accounts/1', json={'name': 'Johnny Doe', 'currency': '€', 'country': 'CountryName'})
     assert response.status_code == 200
-    assert response.json()["name"] == data["name"]
-    assert response.json()["currency"] == data["currency"]
-    assert response.json()["country"] == data["country"]
-    assert response.json()["balance"] == 0.0
-    assert response.json()["status"] == "Active"
-"""
 
-"""
-# New test: Test for Fetching Account Details with Valid Account Number:
-
-def test_get_account_details(client, test_account):
-    account_number = test_account.account_number
-    response = client.get(f"/accounts/{account_number}")
+# New test 2:
+def test_delete_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<int:id>' page is posted to (DELETE)
+    THEN check the response is valid
+    """
+    response = testing_client.delete('/accounts/1')
     assert response.status_code == 200
-    assert response.json()["name"] == test_account.name
-    assert response.json()["currency"] == test_account.currency
-    assert response.json()["country"] == test_account.country
-"""
 
 
 
